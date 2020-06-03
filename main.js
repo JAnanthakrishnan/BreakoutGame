8919f586707c1c8c61c7f6349d490bef52cc69dd
JAnanthakrishnan <ananthakrishnan2017@gmail.com>
2020-06-03 06:54:23 +0530

    Basic structure done

---
+++ main.js
@@ -0,0 +1,10 @@
+const rulesBtn = document.getElementById("rules-btn");
+const closeBtn = document.getElementById("close-btn");
+const rules = document.getElementById("rules");
+
+rulesBtn.addEventListener('click',()=>{
+    rules.classList.add('show');
+})
+closeBtn.addEventListener('click',()=>{
+    rules.classList.remove('show')
+})
